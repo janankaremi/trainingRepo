fc438b81259d023657e4784b75cd2e5c6a2058ac
janankaremi <124959950+janankaremi@users.noreply.github.com>
2023-02-27 17:39:56 -0800

    Update GitHubPracticeTwo.java
    these changes are from upstream github

--- src/test/git/GitHubPracticeTwo.java
+++ src/test/git/GitHubPracticeTwo.java
@@ -4,5 +4,11 @@ public class GitHubPracticeTwo {
 	public int rams(int q, int o) {
 		return q*o;
 	}
+	
+	public int ramst2(int e, int u, String messages){
+		system.out.printlin(messages);
+		int t = e*u;
+		return t;
+	}		
 
 }
